2a9f53be4d62922d32e6eead68bdb2e3e9a6d541
Bryce Laxson <blaxson@calpoly.edu>
2020-08-19 20:09:47 -0700

    added method to update the weights and biases of the NN. added more documentation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,8 +1,14 @@
+import random
 import numpy as np
 
 class NeuralNetwork(object):
+   
+    """ The list 'sizes' contains the num of neurons in the respective layers of 
+        the neural network. If the list is [24, 16, 10], then it would be a 3-layer
+        NN with 24 nodes in the first layer, 16 in the second, etc. """
     def __init__(self, sizes):
-        self.num_layers: = len(sizes)
+
+        self.num_layers = len(sizes)
         self.sizes = sizes
         # biases and weights stored as lists of matrices
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
@@ -40,19 +46,30 @@ class NeuralNetwork(object):
             random.shuffle(training_data)
             # partition all data into batches of data
             batches = [
-                training_data[j : j + batch_size] for j in range(0, n, batch_size)
-                ]
+                training_data[j : j + batch_size] for j in range(0, len_training, batch_size)]
+            # go thru each batch & apply gradient decent in backpropogation
             for batch in batches:
-                # go thru each batch & apply gradient decent in backpropogation
                 self.update_weights_and_biases(batch, l_rate)
             if test_data:
                 print("Epoch {0}: {1} / {2}".format(
-                    i, self.evaluate(test_data), len_test)
+                    i, self.evaluate(test_data), len_test))
             else:
                 print("Epoch {0} complete".format(i)) 
         
+    """ updates the neural network's weights and biases by applying gradient
+        descent using backpropogation on a single batch of test data. 'batch'
+        is a list of tuples (x, y) and 'l_rate' is the learning rate """
     def update_weights_and_biases(self, batch, l_rate):
-        
+        gradient_b = [np.zeros(bias.shape) for bias in self.biases]
+        gradient_w = [np.zeros(weight.shape) for weight in self.weights]
+        for x, y in batch:
+            delta_gradient_b, delta_gradient_w = self.backpropogate(x, y)
+            gradient_b = [gb+dgb for gb, dgb in zip(gradient_b, delta_gradient_b)]
+            gradient_w = [gw+dgw for gw, dgw in zip(gradient_w, delta_gradient_w)]
+        self.weights = [w - (l_rate / len(batch)) * gw 
+                        for w, gw in zip(self.weights, gradient_w)]
+        self.biases =  [b - (l_rate / len(batch)) * gb 
+                        for b, gb in zip(self.biases, gradient_b)]
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
