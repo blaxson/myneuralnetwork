2a6b2a9272171da5fd6655dbf0551a46e8981268
Bryce Laxson <blaxson@calpoly.edu>
2020-09-01 15:34:36 -0700

    small refactor of NN

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -27,6 +27,16 @@ class NeuralNetwork(object):
             x = sigmoid(np.dot(weight, x) + bias)
         return x 
 
+    """Returns the number of test inputs for which the neural
+       network outputs the correct result. The neural
+       network's output is the index of whichever
+       neuron in the final layer has the highest activation."""
+    def evaluate(self, test_data):
+
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                    for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
     """ Trains the neural network using stochastic gradient descent using
         dataset batches. 'trainging_data' is a list of tuples (x, y) containing 
         the 'x' training inputs and the 'y' correct desired outputs. 'epochs' is 
@@ -71,11 +81,11 @@ class NeuralNetwork(object):
         self.biases =  [b - (l_rate / len(batch)) * gb 
                         for b, gb in zip(self.biases, gradient_b)]
     
+    """Return a tuple ``(nabla_b, nabla_w)`` representing the
+       gradient for the cost function C_x.  ``nabla_b`` and
+       ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+       to ``self.biases`` and ``self.weights``."""
     def backpropogate(self, x, y):
-        """Return a tuple ``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         # feedforward
@@ -106,23 +116,14 @@ class NeuralNetwork(object):
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
 
-    def evaluate(self, test_data):
-        """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
-        test_results = [(np.argmax(self.feedforward(x)), y)
-                        for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
-
+    """Return the vector of partial derivatives \partial C_x /
+       \partial a for the output activations."""
     def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
         return (output_activations-y)
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+"""Derivative of the sigmoid function"""
 def sigmoid_deriv(z):
-    """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
